3a2920c85dcbeb248d93f2bc166d4af0c401fa76
sqz02345 <saqibzaheer144@gmail.com>
2025-08-11 21:43:41 -0500

    Update ascii-script.sh

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -3,6 +3,6 @@ echo "hello world"
 ls - ltra
 sudo apt-get update
 sudo apt-get install -y cowsay
-run: cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
+cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
 grep -i "dragon" dragon.txt
 ls - ltra
